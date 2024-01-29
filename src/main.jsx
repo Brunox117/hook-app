e44d6e44d6fb0ef97c691c922f567dec9133a87c
Brunox117 <omarjm0910@gmail.com>
2024-01-29 13:59:53 -0600

    Inicio de useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { TodoApp } from "./08-useReduce/TodoApp";
+import { BrowserRouter } from "react-router-dom";
+// import { TodoApp } from "./08-useReduce/TodoApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
@@ -15,9 +16,13 @@ import { TodoApp } from "./08-useReduce/TodoApp";
 // import { Padre } from "./07-tarea-memo/Padre";
 // import './08-useReduce/intro-reducer';
 // import { HooksApp } from "./HooksApp";
+import { MainApp } from "./09-useContext/MainApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
   // <React.StrictMode>
-  <TodoApp />
   // </React.StrictMode>
 );
+
